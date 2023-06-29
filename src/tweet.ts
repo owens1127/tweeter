28f82212ba317b651a7246e237456360bde72ca1
owensimpson <owensimpson1127@gmail.com>
2023-06-29 15:55:20 -0400

    added banned topics and more adverbs

--- src/tweet.ts
+++ src/tweet.ts
@@ -1,16 +1,20 @@
 import * as dotenv from "dotenv";
-import {
-  ChatCompletionRequestMessageRoleEnum,
-  Configuration,
-  OpenAIApi,
-} from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import { TwitterApi } from "twitter-api-v2";
 import fs from "fs";
 import config from "./config.json";
 
 dotenv.config();
 
-const { username, moods, adverbs, temperature, model } = config;
+const {
+  username,
+  moods,
+  adverbs,
+  temperature,
+  model,
+  bannedTopics,
+  choices: n,
+} = config;
 
 const openai = new OpenAIApi(
   new Configuration({
@@ -33,15 +37,19 @@ async function main() {
   await twitter.tweet(tweet);
 }
 
-async function generateTweet() {
-  const tweets = (
-    JSON.parse(
-      fs.readFileSync(`./collection/tweets_${username}.json`, "utf8")
-    ) as string[]
-  )
+function processTweets(tweets: string[]): string[] {
+  return tweets
     .filter((tweet) => !tweet.includes("http"))
     .map((tweet) => tweet.replace("\n", " "))
     .map((tweet) => tweet.replace(/\s{2,}/g, " "));
+}
+
+async function generateTweet() {
+  const tweets = processTweets(
+    JSON.parse(
+      fs.readFileSync(`./collection/tweets_${username}.json`, "utf8")
+    ) as string[]
+  );
 
   let indices = new Set<number>();
   let selectedTweets = new Set<string>();
@@ -72,6 +80,13 @@ async function generateTweet() {
     model,
     temperature,
     tweets: [...selectedTweets],
+    n,
+  };
+
+  const tweetsPromise = openai.createChatCompletion({
+    model: input.model,
+    n: input.n,
+    temperature: input.temperature,
     messages: [
       {
         role: "system",
@@ -85,22 +100,48 @@ async function generateTweet() {
         role: "user",
         content: `Please generate 1 new ${adverb} ${mood} short tweet based on the writing-style, language, emotion, capitalization, and topics in those tweets`,
       },
-    ] as { role: ChatCompletionRequestMessageRoleEnum; content: string }[],
-  };
-
-  const completion = await openai.createChatCompletion({
-    model: input.model,
-    temperature: input.temperature,
-    messages: input.messages,
+    ],
   });
 
-  const output = completion.data;
-  const tweet = output
-    .choices![0].message!.content!.replace(/#\w+\s?/g, "")
-    .replace("#", "")
-    .trim();
+  const output = (await tweetsPromise).data;
+  const choices = output.choices!.map((choice) =>
+    choice
+      .message!.content!.replace(/#\w+\s?/g, "")
+      .replace("#", "")
+      .trim()
+  );
+  const verificationPromise = openai.createChatCompletion({
+    model: input.model,
+    temperature: input.temperature / 3,
+    n,
+    messages: [
+      {
+        role: "system",
+        content: `You will give a score 0-10 for each item based on the criteria in the prompt. You should response with an array of numbers representing the scores.`,
+      },
+      {
+        role: "user",
+        content: `Please determine if any of the following messages discuss these topics. Here are the topics: ${bannedTopics}. Please rate each tweet on a scale of 1-10 where 1 is related to any of the topics and 10 is not related at all to any: ${choices}`,
+      },
+    ],
+  });
 
-  const log = JSON.stringify({ input, output, tweet }, null, 2);
+  const ratingsByChoice = (await verificationPromise).data.choices.map(
+    (choice) => JSON.parse(choice.message!.content!) as number[]
+  );
+
+  const avgRatings = ratingsByChoice
+    .reduce(
+      (sum, cv) => sum.map((val, i) => val + cv[i]),
+      Array(ratingsByChoice[0].length).fill(0) as number[]
+    )
+    .map((sum) => sum / ratingsByChoice.length);
+
+  const tweet = choices.filter(
+    (_, idx) => avgRatings[idx] >= (1.5 - temperature) / 2
+  )[0];
+
+  const log = JSON.stringify({ input, choices, avgRatings, tweet }, null, 2);
   fs.writeFileSync(
     `./logs/tweet_${username}_${new Date()
       .toISOString()
